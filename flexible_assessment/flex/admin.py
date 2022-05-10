7fafaa27bbea215ff284732091ebb6f2858d8531
Vismai <vismai.khanderao@ubc.ca>
2022-05-10 14:05:29 -0700

    Added db model

--- flexible_assessment/flex/admin.py
+++ flexible_assessment/flex/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Course, UserCourse, Assessment, AssessmentCourse, FlexAssessment
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(UserCourse)
+admin.site.register(Assessment)
+admin.site.register(AssessmentCourse)
+admin.site.register(FlexAssessment)
--- flexible_assessment/flex/models.py
+++ flexible_assessment/flex/models.py
@@ -1,3 +1,40 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    role = models.CharField(max_length=75)
+
+# TODO: add flexible assessment deadline date
+class Course(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=75)
+
+class UserCourse(models.Model):
+    class Meta:
+        constraints = [
+            models.constraints.UniqueConstraint(fields=['user_id', 'course_id'], name='User and Course unique')
+        ]
+
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class Assessment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    title = models.CharField(max_length=75)
+    default = models.FloatField()
+
+class AssessmentCourse(models.Model):
+    class Meta:
+        constraints = [
+            models.constraints.UniqueConstraint(fields=['assessment_id', 'course_id'], name='Assessment and Course unique')
+        ]
+
+    assessment_id = models.ForeignKey(Assessment, on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class FlexAssessment(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    assessment_id = models.ForeignKey(Assessment, on_delete=models.CASCADE)
+    flex = models.FloatField()
+
