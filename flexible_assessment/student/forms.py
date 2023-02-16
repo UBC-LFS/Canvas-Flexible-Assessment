62b0925b54a2db92a69c26fb311e2e028e06fdca
William <william.kang@ubc.ca>
2023-02-16 11:56:35 -0800

    UI: Increased student comment length to 350 chars

--- flexible_assessment/student/forms.py
+++ flexible_assessment/student/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from django.utils import timezone
+from django.utils.html import format_html
 from flexible_assessment.models import Course, UserComment
 from instructor.forms import StudentAssessmentBaseForm
 
+class CharacterCountTextarea(forms.Textarea):
+    def __init__(self, max_length, *args, **kwargs):
+        self.max_length = max_length
+        super().__init__(*args, **kwargs)
+
+    def render(self, name, value, attrs=None, renderer=None):
+        rendered = super().render(name, value, attrs, renderer)
+        count_html = format_html('<p class="mb-0 text-muted">Maximum of {} characters</p>', self.max_length)
+        return rendered + count_html
 
 class StudentAssessmentForm(StudentAssessmentBaseForm):
     def __init__(self, *args, **kwargs):
@@ -15,13 +25,17 @@ class StudentAssessmentForm(StudentAssessmentBaseForm):
             initial_comment = user_comment.comment
         else:
             initial_comment = None
-            
+        
+        comment_length = 350
         fields['comment'] = forms.CharField(
-            max_length=100,
+            max_length=comment_length,
             initial=initial_comment,
-            widget=forms.Textarea(
+            widget=CharacterCountTextarea(
+                max_length=comment_length,
                 attrs={
-                    'rows': 3
+                    'rows': 3,
+                    'class': 'form-control',
+                    'placeholder': 'Enter your comment here',
                 }),
             required=False)
 
