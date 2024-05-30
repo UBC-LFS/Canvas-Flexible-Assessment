390dba08bbd4cdeee65ed0ee2f0061ffa363e8ab
aydenthekinch <aydenkinchla@gmail.com>
2024-05-30 11:46:31 -0700

    Added flat grading, still need to include rule functionality

--- flexible_assessment/flexible_assessment/tests/mock_classes.py
+++ flexible_assessment/flexible_assessment/tests/mock_classes.py
@@ -106,6 +106,11 @@ class MockFlexCanvas(MockCanvas):
         dict = {k: v.asdict() for k, v in self.groups_dict.items()}
         return dict, {} 
 
+    #TODO: Make this work legit in the Mock Canvas enviroment
+    def get_flat_groups_and_enrollments(self, course_id):
+        dict = {k: v.asdict() for k, v in self.groups_dict.items()}
+        return dict, {} 
+
     def is_allow_override(self, course_id):
         return True
     
--- flexible_assessment/instructor/canvas_api.py
+++ flexible_assessment/instructor/canvas_api.py
@@ -105,14 +105,6 @@ class FlexCanvas(Canvas):
         group_id: _id
         group_name: name
         group_weight: groupWeight
-        assignment_list assignmentsConnection {
-          assignments: nodes {
-            assignment_name: name
-            assignment_grade_list: submissionsConnection {
-              assignment_grades: nodes {
-                score
-                user_id: userId
-              }
         grade_list: gradesConnection {
           grades: nodes {
             current_score: currentScore
@@ -158,6 +150,122 @@ class FlexCanvas(Canvas):
                 updated_grades.append((user_id, score))
 
             group_data["grade_list"]["grades"] = updated_grades
+
+        return group_dict, user_enrollment_dict
+
+    def get_flat_groups_and_enrollments(canvas, course_id):
+        """Gets Canvas assignment groups and student enrollment data
+
+        Parameters
+        ----------
+        course_id : int
+            Canvas course ID
+
+        Returns
+        -------
+        group_dict : dict
+            Contains assignment group and grades data
+        user_enrollment_dict : dict
+            Contains enrollment ID for each user
+        """
+
+        query = """
+        query AssignmentGroupQuery($course_id: ID) {
+            course(id: $course_id) {
+                assignment_groups: assignmentGroupsConnection {
+                    groups: nodes {
+                        group_id: _id
+                        group_name: name
+                        group_weight: groupWeight
+                        assignment_list: assignmentsConnection {
+                            assignments: nodes {
+                                max_score: pointsPossible
+                                name
+                                submission_list: submissionsConnection {
+                                    submissions: nodes {
+                                        score
+                                        user_id: userId
+                                    } 
+                                }
+                            } 
+                        } 
+                    grade_list: gradesConnection {
+                        grades: nodes {
+                            current_score: currentScore
+                                enrollment {
+                                    _id
+                                    user {
+                                        user_id: _id
+                                        display_name: name
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        """
+
+        query_response = canvas.graphql(query, variables={"course_id": course_id})
+        query_flattened = _flatten_dict(query_response)
+        groups = query_flattened.get("data.course.assignment_groups.groups", None)
+        if groups is None:
+            raise PermissionDenied
+
+        group_dict = {}
+        user_enrollment_dict = {}
+        for group in groups:
+            id = group.pop("group_id", None)
+            group_dict[id] = group
+
+        for group_data in group_dict.values():
+            group_flattened = _flatten_dict(group_data)
+            assignments = group_flattened.pop("assignment_list.assignments", None)
+            if assignments is None:
+                raise PermissionDenied
+
+            total_assignments = len(assignments)
+            grades = group_flattened.get("grade_list.grades", [])
+            
+            user_scores = {}
+            for assignment in assignments:
+                assignment_flattened = _flatten_dict(assignment)
+                max_score = assignment_flattened.get("max_score", None)
+                submissions = assignment_flattened.get("submission_list.submissions", None)
+                if max_score == 0 or max_score is None or submissions is None:
+                    total_assignments -= 1
+                    continue
+
+                for submission in submissions:
+                    score = submission["score"]
+                    user_id = submission["user_id"]
+                    if user_id is None or score is None:
+                        raise PermissionDenied
+
+                    flat_score = score / max_score if max_score else 0
+                    user_scores[user_id] = user_scores.get(user_id, 0) + flat_score
+
+            updated_grades = []
+            for grade in grades:
+                grade_flattened = _flatten_dict(grade)
+                user_id = grade_flattened.get("enrollment.user.user_id", None)
+                enrollment_id = grade_flattened.get("enrollment._id", None)
+                if user_id is None:
+                    raise PermissionDenied
+                user_enrollment_dict[user_id] = enrollment_id
+
+                if user_id in user_scores:
+                    # Calculate the average score and convert it to percentage
+                    flat_group_score = (user_scores[user_id] / total_assignments) * 100 if total_assignments else 0
+                    updated_grades.append((user_id, round(flat_group_score, 2)))
+                else:
+                    # Keep the original score if no submissions were found
+                    score = grade_flattened["current_score"]
+                    updated_grades.append((user_id, score))
+
+            group_data["grade_list"]["grades"] = updated_grades
+            group_data.pop("assignment_list", None)  # Remove assignment list as requested
 
         return group_dict, user_enrollment_dict
 
