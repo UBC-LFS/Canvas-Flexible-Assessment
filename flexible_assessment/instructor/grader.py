2ecf0702a7aba5f904e0267a99e26dbcc334e7d4
Anica1004 <anicamok@gmail.com>
2024-11-13 16:32:38 -0800

    3 Decimal Places

--- flexible_assessment/instructor/grader.py
+++ flexible_assessment/instructor/grader.py
@@ -38,9 +38,9 @@ def get_default_total(groups, student):
     total_weight = sum(Decimal(w) for w in weights)
     overall = overall / total_weight * Decimal(100) if total_weight != 0 else Decimal(0)
 
-    return overall
+    # return overall
 
-    # return round_half_up(overall, 2)
+    return round_half_up(overall, 3)
 
 
 # def get_default_total(groups, student):
@@ -174,8 +174,8 @@ def get_override_total(groups, student, course):
 
     print(f"printing before the rounding {overall}")
     # Use custom rounding function that works with Decimal
-    return overall
-    # return round_half_up(overall, 2)
+    # return overall
+    return round_half_up(overall, 3)
 
 
 def get_averages(groups, course):
