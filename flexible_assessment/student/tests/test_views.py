ce010952b9f3ddf6a17d6b1685151067e7476f1e
Simon Lin <psymon704@gmail.com>
2025-02-21 14:51:11 -0800

    added test to ensure student comment would be processed properly in log

--- flexible_assessment/student/tests/test_views.py
+++ flexible_assessment/student/tests/test_views.py
@@ -116,3 +116,43 @@ class TestViews(TestCase):
 
         self.assertEqual(type(response), TemplateResponse)
         self.assertEqual(response.status_code, 200)
+
+    def test_StudentAssessmentView_comment_format_on_log(self):
+        course_id = 1
+        assessments = Assessment.objects.filter(course_id=course_id)
+        data = {
+            assessments[0].id.hex: 30,
+            assessments[1].id.hex: 30,
+            assessments[2].id.hex: 10,
+            assessments[3].id.hex: 30,
+            "agreement": True,
+            "comment": "My\nMultiline\nComment",
+        }
+
+        # Get student_home first to set up display_name session data
+        student_home = reverse("student:student_home", args=[course_id])
+        response = self.client.get(student_home)
+
+        response = self.client.post(
+            reverse("student:student_form", args=[course_id]), data=data
+        )
+        # A successful form post should redirect the user to the home page
+        self.assertEqual(type(response), HttpResponseRedirect)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response.url, reverse("student:student_home", args=[course_id])
+        )
+
+        # insert test here checking last log entry for comment
+        import os
+        from django.conf import settings
+
+        log_file_path = os.path.join(settings.BASE_DIR, "log", "info.log")
+        with open(log_file_path) as f:
+            for line in f:
+                pass
+            last_line = line
+            self.assertTrue(
+                "- Updated comment to 'My\\nMultiline\\nComment' | test_student1"
+                in last_line
+            )
