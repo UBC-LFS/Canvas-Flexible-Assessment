5cbac87f0c38cf55d35699b6248985694ebb4419
Simon Lin <psymon704@gmail.com>
2025-04-30 12:21:29 -0700

    drafted basic structure of due dates page

--- flexible_assessment/due_dates/urls.py
+++ flexible_assessment/due_dates/urls.py
@@ -5,4 +5,6 @@ app_name = "due_dates"
 
 urlpatterns = [
     path("<int:course_id>/", views.DueDatesHome.as_view(), name='due_dates_home'),
+    path("<int:course_id>/students", views.DueDatesStudents.as_view(), name='due_dates_students'),
+    path("<int:course_id>/quizzes", views.DueDatesQuizzes.as_view(), name='due_dates_quizzes'),
 ]
--- flexible_assessment/due_dates/views.py
+++ flexible_assessment/due_dates/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
 import flexible_assessment.class_views as views
 import flexible_assessment.utils as utils
+from django.forms import BaseModelFormSet, ValidationError
 
 class DueDatesHome(views.InstructorTemplateView):
     template_name = "due_dates/due_dates_home.html"
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        login_redirect = request.GET.get("login_redirect")
+        if login_redirect:
+            course = self.get_context_data().get("course", "")
+            utils.update_students(request, course)
+        return response
+    
+class DueDatesStudents(views.InstructorFormView):
+    template_name = "due_dates/due_dates_students.html"
+    form_class = BaseModelFormSet
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        login_redirect = request.GET.get("login_redirect")
+        if login_redirect:
+            course = self.get_context_data().get("course", "")
+            utils.update_students(request, course)
+        return response
+    
+class DueDatesQuizzes(views.InstructorFormView):
+    template_name = "due_dates/due_dates_quizzes.html"
+    form_class = BaseModelFormSet
 
     def get(self, request, *args, **kwargs):
         response = super().get(request, *args, **kwargs)
